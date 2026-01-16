6f2d8c7f1201f8c26fc54822b1c23c17239bf4d5
Samuel <oluwamuyiwasamuel08@gmail.com>
2026-01-16 14:09:14 +0100

    Completed Part 4 Exercise 18: PersonalInformation

---
+++ Part 4/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonalInformationCollection {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the PersonalInformation class
+
+        ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            if(firstName.isEmpty()){
+                break;
+            }
+
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+            if(lastName.isEmpty()){
+                break;
+            }
+
+            System.out.print("Identification number: ");
+            String idNum = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNum));
+        }
+        System.out.println();
+        for(PersonalInformation each : infoCollection){
+            System.out.println(each.getFirstName() + " " + each.getLastName() + " ");
+        }
+
+        scanner.close();
+    }
+}
